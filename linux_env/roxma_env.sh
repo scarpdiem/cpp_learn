798518a0633d492952b884e5685c4f2048706f8d
roxma <roxma@qq.com>
2014-10-10 22:17:19 +0800

    update make_print_include_path_for_ctags, trace directory changes

--- linux_env/roxma_env.sh
+++ linux_env/roxma_env.sh
@@ -22,10 +22,10 @@ function getRoxVimrcFile(){
 		hi TabLineFill     term=reverse cterm=reverse
 
 		" folding
-		set foldenable
-		set foldmethod=syntax
-		set foldlevel=100
-		"autocmd BufNewFile,BufRead *.cpp,*.c,*.h,*.hpp  set foldmethod=syntax
+		" set foldenable
+		" set foldmethod=syntax
+		" set foldlevel=100
+		autocmd BufNewFile,BufRead *.cpp,*.c,*.h,*.hpp  set foldmethod=syntax
 
 		" indentation
 		set tabstop=4
@@ -42,8 +42,8 @@ EOFVIMRC
 	echo "$roxVimrcFile"
 }
 
-unalias vim
-alias vim="$(which vim | awk '{print $NF}') -S $(getRoxVimrcFile)"
+unalias vim 2>/dev/null
+alias vim="$(which vim | awk '{print $NF}') --cmd \"source $(getRoxVimrcFile)\""
 # alias rvim='vim -S $(getRoxVimrcFile)'
 
 function cdl(){
@@ -55,14 +55,51 @@ function cdl(){
 #   for example: make_print_include_path_for_ctags make all
 # author: roxma
 function make_print_include_path_for_ctags(){
-	# print make command | filter compiling command | filter include options | filter include path
-	local includePath=$( $@ --just-print | grep -P '^(gcc|g\+\+)' | grep -ohP '\s\-I\s*[\S]+'  | sed "s/^\s\-I//" )
-		# The following is not a strict filter, but OK for ctags
-	# local defaultPath=$( g++ -E -x c++ - -v < /dev/null 2>&1 | grep -ohP '^\s\/\S+' )
-		local defaultPath=""
-	local allPath="${includePath} ${defaultpath}"
-	# echo all absolute path | ... sort and delete duplicate path
-	echo $(for p in $allPath ; do echo "$(readlink -f $p)" ; done) | xargs -n1 | sort -u | xargs
+	# # The following is not a strict filter, but OK for ctags
+	# # local defaultPath=$( g++ -E -x c++ - -v < /dev/null 2>&1 | grep -ohP '^\s\/\S+' )
+	
+	local dirStack=("$(readlink -f .)")
+
+	# print the command, and read the output line by line
+	$@ 2>&1 3>&1 | while read -r line ; do
+
+		# match string, for example:
+		# make[1]: Entering directory `/home/roxma/test/src'
+		if [[ "$line" =~ [[:alnum:]]*make[[:alnum:]]*\[[[:digit:]+]\]\:[[:blank:]](Entering)[[:blank:]]directory[[:blank:]]  ]]
+		then
+			# take /home/roxma/test/src from the matched string
+			local dir=$(echo "$line" | grep -o -P "(?<=\`).*(?=')")
+			# echo "$line"
+			dirStack+=("$dir")
+		fi
+
+		# match string, for example:
+		# make[1]: Leaving directory `/home/roxma/test/src'
+		if [[ "$line" =~ [[:alnum:]]*make[[:alnum:]]*\[[[:digit:]+]\]\:[[:blank:]](Leaving)[[:blank:]]directory[[:blank:]]  ]]
+		then
+			# take /home/roxma/test/src from the matched string
+			local dir=$(echo "$line" | grep -o -P "(?<=\`).*(?=')")
+			# echo "$line"
+			unset dirStack[${#dirStack[@]}-1]
+		fi
+		# local currentBase=
+		
+		local curDir="${dirStack[${#dirStack[@]}-1]}"
+		# echo "$curDir"
+
+		# if is compilation command
+		if [[ "$line" =~ ^[[:blank:]]*(gcc|g\+\+) ]]
+		then
+			local incOptions=$(echo "$line" | grep -ohP '\s\-I\s*[\S]+'  | sed "s/^\s\-I//")
+			for incOption in $incOptions
+			do
+				local incPath="$curDir/${incOption// }"
+				incPath=`readlink -f $incPath`
+				echo "$incPath"
+			done
+		fi
+		
+	done | xargs -n1 | sort -u | xargs # remove duplicate words
 }
 
 
