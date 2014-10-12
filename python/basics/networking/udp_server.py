3c3e8558bc51ca3646a6e4580e17d264504e0e69
roxma <roxma@qq.com>
2014-10-12 18:18:38 +0800

    python add networkiong

---
+++ python/basics/networking/udp_server.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+# This program receive a udp packet and then exit
+
+def Entry():
+
+	port = 12345
+	maxReceiveSize = 1024
+	timeout = 60
+	
+	import socket
+	udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	udp.bind(("",port))
+	udp.settimeout(timeout)
+	buffer, address = udp.recvfrom(maxReceiveSize)
+	
+
+	print("address:" + str(address))
+	print("content size = " + str(len(buffer)))
+	print(buffer)
+	
+	
+
+
+
+Entry()
+
