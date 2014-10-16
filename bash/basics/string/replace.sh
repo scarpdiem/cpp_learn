e424be6bf79470fb57ba9f4380858846a22649a0
roxma <roxma@qq.com>
2014-10-17 01:25:35 +0800

    replace.sh

--- bash/basics/string/replace.sh
+++ bash/basics/string/replace.sh
@@ -8,7 +8,7 @@ echo "2. Prefix remved:"
 message="Hello world"
 echo "${message/#Hello/}"
 
-echo "3. substutute:"
+echo "3. replace:"
 message="Hello, my name is my_name, nice to meet you"
 echo "${message/my_name/roxma}"
 
