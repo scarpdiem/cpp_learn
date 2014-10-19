86e763c6ea01d7ca5dbcf75e814c07845962a07a
roxma <roxma@qq.com>
2014-10-20 01:08:40 +0800

    add mysql/insert/duplicate_key.sql

---
+++ mysql/insert/duplicate_key.sql
@@ -0,0 +1,17 @@
+
+# mysql -uroot --database=test -e 'source duplicate_key.sql'
+
+create table duplicate_test(id bigint primary key, cnt bigint not null)engine=Innodb, charset=utf8;
+
+insert into duplicate_test(id, cnt) values(1,1), (2,2), (3, 3);
+
+select * from duplicate_test;
+
+insert into duplicate_test(id, cnt) values(1,5) on duplicate key update cnt=cnt+values(cnt);
+
+insert into duplicate_test(id, cnt) values(2,6),(3,7) on duplicate key update cnt=cnt+values(cnt);
+
+select * from duplicate_test;
+
+drop table duplicate_test;
+
