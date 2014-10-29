206d68fb34afb483cea0e295905a8dd2e4e163c9
roxma <roxma@qq.com>
2014-10-29 11:34:51 +0800

    add mysql/admin/size.sql

---
+++ mysql/admin/size.sql
@@ -0,0 +1,12 @@
+
+# get the size of tables in current database
+SELECT DATABASE() FROM DUAL into @dbName;
+SELECT 
+				table_name AS "Tables", round(((data_length + index_length) / 1024 / 1024), 2) as size_mb, round((data_free)/1024/1024,2) as free_mb FROM information_schema.TABLES WHERE table_schema = @dbName 
+	union all
+		select "All"                  , round(sum(((data_length + index_length) / 1024 / 1024)),2) as size_mb, round((data_free)/1024/1024,2) as free_mb  FROM information_schema.TABLES WHERE table_schema = @dbName   
+	ORDER BY size_mb DESC;
+
+# get the size of databases
+select table_schema as database_name, round(sum(((data_length + index_length) / 1024 / 1024)),2) as size_mb, round((data_free)/1024/1024,2) as free_mb from information_schema.TABLES group by table_schema;
+
