e1927d896d819ae67122e847c1bbdbcb19877fdf
roxma <roxma@qq.com>
2014-11-05 20:18:01 +0800

    update vim NOTE

--- linux_env/vim/README.mk
+++ linux_env/vim/README.mk
@@ -1,29 +1,58 @@
 
-if compile vim from source, then:
+
+
+
+# Environment setup
+
+Compile vim from source with:
 
 	./configure --prefix=$(readlink -f ~/roxma/.local_vim)/ --with-features=huge  --enable-pythoninterp && make && make install
 
 
-if compile cmake from source:
+Compile cmake from source with:
 
 	mkdir ~/.local_cmake
 	./configure --prefix=$(readlink -f ~/.local_cmake)/
 	make && make install
 
 
-List of useful commands:
 
 
-### Normal mode
+# List of useful commands:
+
+
+### Normal mode Movint
 
 - Ctrl-O	Jump back to the previous (older) location. Use the ':jumps' command to see a list of jump history.
 - Ctrl-I	(same as Tab) Jump forward to the next (newer) location.
+
 
 ### Command Line editing
 
 - Ctrl-B	Move to beginning of command line.
 - Ctrl-E	Move to end of command line.
-- Ctrl-W	Delete the word before the cursor.
-- Ctrl-U	Delete the word before the cursor.
+- Ctrl-W	Delete the word before the cursor.  - Ctrl-U	Delete the word before the cursor.
 
 - Ctrl-F	Open command line window.
+
+
+### Search and Replace
+
+#### Replace one by one
+
+`:s/search_string/replace_string/` to replace the first string found in the cursor line, Then `n` to find the next, then `&` to repeat the last substitute command.
+
+- `s`	substitute
+	
+#### Replace all
+
+`:%s/search_string/replace_string/g`
+
+- `%`	Traverse and Execute all lines
+- `g`	Replace all occurance in the line.
+
+#### Replace inside blocks
+
+`v` or `V`, and then `:s/search_string/replace_string/`
+	
+
