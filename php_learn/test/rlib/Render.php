6f75d563ad0b5e1ee363c888038111847e3610b7
roxma <roxma@qq.com>
2014-11-09 16:27:16 +0800

    rename

--- php_learn/test/rlib/Render.php
+++ php_learn/test/rlib/Render.php
@@ -1,10 +1,10 @@
 <?php
 
-require_once dirname(__FILE__) . "/" .  '../../libs/rlib/Render.php';
+require_once dirname(__FILE__) . "/" .  '../../libs/rlib/RenderPhpToString.php';
 
 function Entry(){
 	$output = "";
-	Render( dirname(__FILE__) . "/" . "Render.tpl.php", array("msg"=>"hello world"), $output);
+	RenderPhpToString( dirname(__FILE__) . "/" . "Render.tpl.php", array("msg"=>"hello world"), $output);
 
 	var_dump($output);
 
