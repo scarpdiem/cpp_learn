155caa1b2668e2afa2600d2d01a20d4acd98a902
roxma <roxma@qq.com>
2014-11-25 21:03:23 +0800

    require.mk, rearrange directory

---
+++ cpp/rlib/require.mk
@@ -0,0 +1,16 @@
+
+REQUIRE_CURDIR := $(dir $(abspath $(lastword $(MAKEFILE_LIST))))
+
+REQUIRE_INC += $(REQUIRE_CURDIR)/src
+REQUIRE_LIB += $(REQUIRE_CURDIR)/bin/librlib.a
+
+# This library is built from source
+
+all: $(REQUIRE_CURDIR)/bin/librlib.a
+$(REQUIRE_CURDIR)/bin/librlib.a:
+	$(MAKE) -C $(REQUIRE_CURDIR)/src
+
+clean_require: $(REQUIRE_CURDIR)/clean
+$(REQUIRE_CURDIR)/clean:
+	$(MAKE) -C $(REQUIRE_CURDIR)/src clean
+
