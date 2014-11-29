e334f785d5aa109f313cdec26b7c39c1836f6a46
roxma <roxma@qq.com>
2014-11-30 02:10:58 +0800

    update makefile python example

--- cpp/makefile/python/test.mk
+++ cpp/makefile/python/test.mk
@@ -3,7 +3,11 @@ define pythonCode
 print "hello"
 endef
 
+define PrintMessagePy
+print "$(1)"
+endef
 
 all:
-	echo $(shell python -c '$(pythonCode)')
+	@echo $(shell python -c '$(pythonCode)')
+	@echo $(shell python -c '$(call PrintMessagePy,hello world)')
 
