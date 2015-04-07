ce513ce8ceda0d897c49b26cd17d01b02ab3907c
roxma <roxma@qq.com>
2015-04-08 00:15:51 +0800

    organize

--- linux_env/vim/init.sh
+++ linux_env/vim/init.sh
@@ -1,30 +1,33 @@
-{
+##
+# This script is used to setup the user's vim environment, including:
+# 1. vim command alias
+# 2. update vim plugins
+#
 
+{
 	unalias vim 2>/dev/null
 	customVimrcFile=$(getRoxVimrcFile)
 	alias vim="$(which vim | awk '{print $NF}') --cmd \"source $customVimrcFile\" -p"
-	alias vim 1>&2
 
 	localVimDir=".local_software/local_vim"
 
 	cd $(dirname ${BASH_SOURCE[0]})
 
-	echo "$(dirname ${BASH_SOURCE[0]})" 1>&2
 	if [[ -d $(pwd)/${localVimDir} ]]; then
 
 		# use --cmd option to execute cmd before any vimrc file loaded to make pathogen work
 		unalias vim 2>/dev/null
 		alias vim="$(pwd)/${localVimDir}/bin/vim -u \"$customVimrcFile\" -p"
-		alias vim 1>&2
 
-		# prepend some script to the vimrc file.
+		###
+		# prepend some script to the vimrc file:
+		# 1. set custom vimrc environment variables
+		# 2. add pathogen initialization to the beginning of vimrc file
 		echo "
 			let s:customvimrc_local_software_dir='$(pwd)/.local_software/'
 
-			\" add pathogen initialization to the beginning of vimrc file
 			set rtp+=$(pwd)/${localVimDir}/
 			set rtp+=$(pwd)/${localVimDir}/vim-pathogen-master/
-
 			set nocompatible
 			execute pathogen#infect('$(pwd)/${localVimDir}/bundle/{}')
 			syntax on
@@ -32,14 +35,16 @@
 		" > ${customVimrcFile}.tmp
 		cat ${customVimrcFile} >> ${customVimrcFile}.tmp
 		mv ${customVimrcFile}.tmp ${customVimrcFile}
+		#
+		###
 
 		# If plugins has changed
 		if [[ "$(getPluginsTgzEncodedContentMd5sum)" != "$(cat ${localVimDir}/plugins_md5sum.txt 2>/dev/null )" ]]
 		then
 
-			echo "updating vim plugins" 1>&2
+			echo "updating vim plugins..." 1>&2
 
-			# first clean the old plugins
+			# clean old plugins
 			rm -rf ${localVimDir}/plugins_md5sum.txt ${localVimDir}/plugins ${localVimDir}/plugins.tar.gz ${localVimDir}/bundle ${localVimDir}/vim-pathogen-master
 
 			# decompress new vim plugins
@@ -72,9 +77,13 @@
 		fi
 
 	fi
+
 	cd - 1>&2 # go back
 
 	# ctags
 	unalias ctags 2>/dev/null
 	alias ctags="$(which ctags | awk '{print $NF}') --c-kinds=+p --c++-kinds=+p"
+
+	alias vim 1>&2
+	alias ctags 1>&2
 }
